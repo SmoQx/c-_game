4063a1ff2d5603b4acee533bd030ff00c7e104a4
smoq <smoq@localhost.localdomain>
2024-01-12 13:51:57 +0100

    added collision

--- Game.cs
+++ Game.cs
@@ -18,6 +18,7 @@ public class Game
         playerPosition = new Position(width / 2, height - player.Height);
         elements.Add(new Element(player, playerPosition));
         elements.Add(new Element(new Obstacle(2, 5), new Position(10, 13)));
+        elements.Add(new Element(new Obstacle(2, 2), new Position(8, 13)));
 
         gameState = GameState.MainMenu;
     }
@@ -56,7 +57,7 @@ public class Game
                     {
                         element.Render();
                     }
-                    map.Render();
+                    //map.Render();
                     var key = ConsoleKey.NoName;
                     if (Console.KeyAvailable)
                         key = Console.ReadKey().Key;
@@ -75,8 +76,10 @@ public class Game
                         case ConsoleKey.Spacebar:
                             Jump(player.VelocityY);
                             break;
+                        case ConsoleKey.DownArrow:
+                            Jump(-player.VelocityY);
+                            break;
                     }
-                    CollidesWith();
 
                     await Task.Delay(10); // Adjust the delay to control the speed of the game
 
@@ -92,6 +95,9 @@ public class Game
     private void Move(int x)
     {
         int newX = elements[0].Pos.X + x;
+        int oldX = playerPosition.X;
+        if (CollidesWith(new List<int>{playerPosition.X + x, playerPosition.Y}) != 3)
+            
         if (newX >= 0 && newX <= gamesizeX - player.Width)
         {
             playerPosition.X = newX;
@@ -102,6 +108,8 @@ public class Game
     {
         int veloY = y;
         int newY = playerPosition.Y - veloY;
+        if (CollidesWith(new List<int>{playerPosition.X, playerPosition.Y - y}) != 3)
+        
         if (newY <= 50)
         {
             playerPosition.Y = newY;
@@ -114,27 +122,25 @@ public class Game
         return 0;
     }
 
-    private void CollidesWith()
+    private int CollidesWith(List<int> pos)
     {
-        Console.Write(playerPosition.X);
-        Console.WriteLine(playerPosition.Y);
         var colidesX = false;
         var colidesY = false;
+        var colides = 0;
         foreach (var element in elements)
         {
             if (element == elements[0])
                 continue;
             if (
-                playerPosition.X + player.Width > element.Pos.X
-                && playerPosition.X < element.Pos.X + element.Object.Width
+                pos[0] + player.Width > element.Pos.X
+                && pos[0] < element.Pos.X + element.Object.Width
             )
             {
                 colidesX = true;
             }
-            Console.WriteLine(element.Pos.Y + element.Object.Height - 1);
             if (
-                playerPosition.Y < element.Pos.Y + element.Object.Height
-                && playerPosition.Y + player.Height - 1 >= element.Pos.Y
+                pos[1] < element.Pos.Y + element.Object.Height
+                && pos[1] + player.Height - 1 >= element.Pos.Y
             )
             {
                 colidesY = true;
@@ -142,7 +148,9 @@ public class Game
             if (colidesY && colidesX)
             {
                 Console.Write("colides");
+                return 3;
             }
         }
+        return colides;
     }
 }
